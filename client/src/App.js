944510d027ae06f8bef866e37258b87b8c7e1dba
lokendra-rawat <lokendra.rawat@galaxycard.in>
2019-01-14 13:03:56 +0530

    added all files again

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import OtherPage from './Otherpage'
+import Fib from './Fib'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            {/* <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+          </p> */}
+            <Link to="/"> home </Link>
+            <Link to="/otherpage"> otherpage </Link>
+
+            <div>
+              <Route exact path="/" component={Fib} />
+              <Route path="/otherpage" component={OtherPage} />
+            </div>
+          </header>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
